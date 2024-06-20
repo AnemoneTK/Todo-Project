a717d3dfa5da2462023dcf39184cf51d7ca97b6b
AnemoneTK <Copp.teenkung@gmail.com>
2024-06-21 02:11:18 +0700

    [EDIT]-edit col responsive

--- React-todo/src/App.jsx
+++ React-todo/src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <>
       <div className="warp container">
-        <div className="box col-12 col-md-10 col-lg-6 rounded-4">
+        <div className="box col-12 col-md-10 col-lg-10 col-xl-6 rounded-4">
           <InputBox />
           <TodoList />
         </div>
