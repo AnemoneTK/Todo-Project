3f6c71ddf508d80e35cbfc995892d4d150e9e04a
AnemoneTK <Copp.teenkung@gmail.com>
2024-07-16 22:44:54 +0700

    [CONFIG]-config router for edit page at route param

--- Vue-todo/src/App.vue
+++ Vue-todo/src/App.vue
@@ -1,5 +1,14 @@
 <template lang="">
   <div>
+    <router-link to="/">
+    <button class="border px-2 mx-1">Home</button>
+    </router-link>
+    <router-link to="/create">
+    <button class="border px-2 mx-1">Create</button>
+    </router-link>
+    <router-link to="/edit/1">
+    <button class="border px-2 mx-1">Edit</button>
+    </router-link>
     <router-view></router-view>
   </div>
 </template>
--- Vue-todo/src/pages/Edit.vue
+++ Vue-todo/src/pages/Edit.vue
@@ -1,13 +1,23 @@
-<template lang="">
+<template>
     <div>
-        <h1>Edit</h1>
+      <h1>Edit Task {{ taskId }}</h1>
+      <!-- Form for editing task with taskId -->
     </div>
-</template>
-<script>
-export default {
-    name:'Home'
-}
-</script>
-<style lang="">
-    
-</style>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, ref } from 'vue';
+  import { useRoute } from 'vue-router';
+  
+  export default defineComponent({
+    name: 'Edit',
+    setup() {
+      const route = useRoute();
+      const taskId = ref<number | string>(route.params.id as number | string);
+  
+      return {
+        taskId,
+      };
+    },
+  });
+  </script>
